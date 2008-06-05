4364d205c5587d020b1cbfb98e10a378a8cb882b
rkaminski <rkaminski@3e02f596-3131-0410-8352-e04eeaec37cc>
2008-06-05 13:11:10 +0000

    - improved the script it is much faster now   i am using awk to update the symtab now
    git-svn-id: https://potassco.svn.sourceforge.net/svnroot/potassco/branches/gringo2@299 3e02f596-3131-0410-8352-e04eeaec37cc

--- bin/check.sh
+++ bin/check.sh
@@ -1,34 +1,68 @@
 #!/bin/bash
 
-# this belongs by far to the most evil shell scripts i have ever written :)
-
-function symtab()
-{
-	begin=$[$(cat $1 | grep -n "^0$" | head -n 1 | grep -o "^[0-9]*") + 1]
-	end=$[$(tail -n +$begin $1 | grep -n "^0$" | head -n 1 | grep -o "^[0-9]*") - 1]
-	cat $1 | tail -n +$begin | head -n $end | sed -e "s/[0-9]* //" | sort > $1.symtab
-}
-
 lparse $* > compare.lparse
 gringo2 -l $* > compare.gringo
 
-symtab compare.lparse
-symtab compare.gringo
+awk '
+func seek0(fin, fout) {
+	do {
+		getline l < fin
+		print l > fout
+	} while( l != "0" )
+}
 
-diff --suppress-common-lines compare.gringo.symtab compare.lparse.symtab | grep "> " | sed -e 's/>//' -e 's/$/$/' > diff1.txt
-diff --suppress-common-lines compare.gringo.symtab compare.lparse.symtab | grep "< " | sed -e 's/<//' -e 's/$/$/' > diff2.txt
+func symtab(fin, fout, a, ka) {
+	j = 1
+	do {
+		getline l < fin
+		if( l != "0" ) {
+			split(l, s, " ")
+			a[s[2]] = s[1]
+			ka[++j] = s[2]
+		}
+	} while( l != "0" )
+	asort(ka)
+	return j - 1
+}
 
-if [ -s diff1.txt ]; then
-	grep -v -f diff1.txt compare.lparse > compare.lparse.reduced
-else
-	cp compare.lparse compare.lparse.reduced
-fi
+func rest(fin, fout) {
+	while ((getline l < fin) > 0) {
+		print l > fout
+	}
+}
 
-if [ -s diff2.txt ]; then
-	grep -v -f diff2.txt compare.gringo > compare.gringo.reduced
-else
-	cp compare.gringo compare.gringo.reduced
-fi
+func intersect(a, ka, la, fa, b, kb, lb, fb)
+{
+	ia = 1;
+	ib = 1;
+	while (ia <= la && ib <= lb) {
+		na = ka[ia]
+		nb = kb[ib]
+		if (na == nb) {
+			print a[na] " " na > fa
+			print b[nb] " " nb > fb
+			ia++;
+			ib++;
+		} else if (na > nb) {
+			ib++;
+		} else {
+			ia++;
+		}
+	}
+	print "0" > fa
+	print "0" > fb
+}
+
+BEGIN {
+	seek0("compare.lparse", "compare.lparse.reduced")
+	seek0("compare.gringo", "compare.gringo.reduced")
+	la = symtab("compare.lparse", "compare.lparse.reduced", a, ka)
+	lb = symtab("compare.gringo", "compare.gringo.reduced", b, kb)
+	intersect(a, ka, la, "compare.lparse.reduced", b, kb, lb, "compare.gringo.reduced")
+	rest("compare.lparse", "compare.lparse.reduced")
+	rest("compare.gringo", "compare.gringo.reduced")
+}
+'
 
 lpeq compare.gringo.reduced compare.lparse.reduced > compare1.lp
 lpeq compare.lparse.reduced compare.gringo.reduced > compare2.lp
@@ -38,7 +72,7 @@ clasp < compare1.lp
 echo "second comparison"
 clasp < compare2.lp
 
-#rm compare.lparse compare.lparse.reduced compare.lparse.symtab 
-#rm compare.gringo compare.gringo.reduced compare.gringo.symtab 
-#rm compare1.lp compare2.lp
-#rm diff1.txt diff2.txt
+rm compare.lparse compare.lparse.reduced 
+rm compare.gringo compare.gringo.reduced 
+rm compare1.lp compare2.lp
+
